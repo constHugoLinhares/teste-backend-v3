40c7b519fc7e4e383f67aabb9731f2fd860c4b72
Hugo Linhares <hugolinhares92@gmail.com>
2025-03-27 21:18:07 -0300

    🧪test: Teste da geração do XML

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -66,5 +66,37 @@ namespace TheatricalPlayersRefactoringKata.Tests
 
             Approvals.Verify(result);
         }
+
+
+        [Fact]
+        public void TestXmlStatementExample()
+        {
+            var plays = new Dictionary<string, Play>
+            {
+                { "hamlet", new Play("Hamlet", 4024, "tragedy") },
+                { "as-like", new Play("As You Like It", 2670, "comedy") },
+                { "othello", new Play("Othello", 3560, "tragedy") },
+                { "henry-v", new Play("Henry V", 3227, "history") },
+                { "john", new Play("King John", 2648, "history") },
+                { "richard-iii", new Play("Richard III", 3718, "history") }
+            };
+
+            Invoice invoice = new Invoice(
+                "BigCo",
+                new List<Performance>
+                {
+                    new Performance(plays["hamlet"], 55),
+                    new Performance(plays["as-like"], 35),
+                    new Performance(plays["othello"], 40),
+                    new Performance(plays["henry-v"], 20),
+                    new Performance(plays["john"], 39),
+                    new Performance(plays["henry-v"], 20)
+                }
+            );
+
+            var result = statementPrinter.GenerateXml(invoice);
+
+            Approvals.Verify(result);
+        }
     }
 }
