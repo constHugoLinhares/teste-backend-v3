584b5593e61263c09d4ad7261457682dc4b6a7b8
Hugo Linhares <hugolinhares92@gmail.com>
2025-03-27 21:13:33 -0300

    ✨feat: Generate XML Printer

--- TheatricalPlayersRefactoringKata/Application/Services/Statement/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/Application/Services/Statement/StatementPrinter.cs
@@ -1,5 +1,8 @@
 ﻿using System.Globalization;
 using System.Text;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System;
 
 namespace TheatricalPlayersRefactoringKata;
 
@@ -34,5 +37,52 @@ public class StatementPrinter
         result.AppendLine($"You earned {volumeCredits} credits");
 
         return result.ToString();
+    }
+
+    public string GenerateXml(Invoice invoice)
+    {
+        CultureInfo cultureInfo = new("en-US");
+        double totalAmount = 0;
+        int totalCredits = 0;
+        var items = new List<XElement>();
+
+        foreach (var performance in invoice.Performances)
+        {
+            int credits = 0;
+            var play = performance.Play;
+            var strategy = PlayPricingStrategyFactory.GetStrategy(play.Genre);
+            double amount = strategy.CalculatePrice(play, performance);
+
+            if (strategy is ICreditsStrategy creditsStrategy)
+            {
+                credits = creditsStrategy.CalculateCredits(performance);
+                totalCredits += creditsStrategy.CalculateCredits(performance);
+            }
+
+            totalAmount += amount;
+
+            items.Add(
+                new XElement("Item",
+                    new XElement("AmountOwed", amount.ToString("0.##", CultureInfo.InvariantCulture)),
+                    new XElement("EarnedCredits", credits),
+                    new XElement("Seats", performance.Audience)
+                )
+            );
+        }
+
+        var doc =
+        new XDocument(
+            new XDeclaration("1.0", "utf-8", null),
+            new XElement("Statement",
+                new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+                new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema"),
+                new XElement("Customer", invoice.Customer),
+                new XElement("Items", items),
+                new XElement("AmountOwed", totalAmount.ToString("0.##", CultureInfo.InvariantCulture)),
+                new XElement("EarnedCredits", totalCredits)
+            )
+        );
+
+        return doc.Declaration + Environment.NewLine + doc.ToString();
     }
 }
